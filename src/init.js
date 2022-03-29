e8fa929d07630a62af83256c68b6f36c75f789d5
bani <13972186114@139.com>
2022-03-29 23:55:59 +0800

    feat:html解析成ast

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import { compileToFunction } from "./compiler/index"
 import { initState } from "./state"
 
 export function initMixin (Vue) {
@@ -5,5 +6,23 @@ export function initMixin (Vue) {
     const vm = this
     vm.$options = option
     initState(vm)
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el)
+    }
+  }
+
+
+  Vue.prototype.$mount = function (el) {
+    el = document.querySelector(el)
+    const vm = this;
+    let options = vm.$options
+    if (!options.render) {
+      let template = options.template;
+      if (!template && el) {
+        template = el.outerHTML;
+        let render = compileToFunction(template)
+        options.render = render
+      }
+    }
   }
 }
